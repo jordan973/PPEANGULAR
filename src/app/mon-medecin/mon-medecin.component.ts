34f21836b7e58efb513874300b6e5b1a4de1b05e
Liamoklm <liamvogel01@gmail.com>
2020-05-04 17:21:45 +0200

    liam fin

--- src/app/mon-medecin/mon-medecin.component.ts
+++ src/app/mon-medecin/mon-medecin.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { MainService } from '../services/main.service';
 import { Subscription } from 'rxjs';
 import { ngModuleJitUrl } from '@angular/compiler';
-
-
+ 
 @Component({
   selector: 'app-mon-medecin',
   templateUrl: './mon-medecin.component.html',
@@ -11,31 +10,45 @@ import { ngModuleJitUrl } from '@angular/compiler';
 })
 export class MonMedecinComponent implements OnInit {
   medecins: any[];
+  rapports : any[];
   nomMedecin = '';
+  rapportSubscription : Subscription;
   headElements = ['nom','prenom','téléphone','adresse','département'];
   
   medecinSubscription: Subscription;
   constructor(private medecinService: MainService){}
   ngOnInit(){
+    this.medecinService.getmedecinFromServer(this.nomMedecin);
     this.medecinSubscription = this.medecinService.medecinSubject.subscribe(
       (medecins: any[]) => {
         this.medecins = medecins;
+        
+       }
+    );
+    this.rapportSubscription = this.medecinService.rapportsSubject.subscribe(
+      (rapports : any[]) => {
+        this.rapports = rapports;
       }
     );
-    this.medecinService.getmedecinFromServer(this.nomMedecin);
     this.rechercheMedecin(event);
-
+    this.medecinService.emitSubject()
+  
+ 
   }
-
+ 
   rechercheMedecin(event) {
     if (!event) {
       this.medecinService.getmedecinFromServer(this.nomMedecin);
     }
-
+ 
     if (event) {
       this.nomMedecin = event.target.value;
       this.medecinService.getmedecinFromServer(this.nomMedecin);
     }
   }
 
+  afficherRapport(med : any){
+    this.medecinService.getRapportFromServer(med.id);
+
+  }
 }
--- src/app/services/main.service.ts
+++ src/app/services/main.service.ts
@@ -7,21 +7,25 @@ import { Subject } from 'rxjs';
 export class MainService{
 
     medecinSubject = new Subject<any[]>();
-    medecins : any[];
+    medecins = [];
+    rapportsSubject = new Subject<any[]>();
+    rapports = [];
+
 
     constructor(private httpClient: HttpClient){}
 
 
-    emitmedecinSubject(){
+    emitSubject(){
         this.medecinSubject.next(this.medecins.slice());
+        this.rapportsSubject.next(this.rapports.slice());
     }
 
     // Récupère un médecin en fonction de son nom
     getmedecinFromServer(nom){
-        this.httpClient.get<any[]>('https://webserv-gr4.sio-carriat.com/gsbapi/?noms='+nom).subscribe(
+        this.httpClient.get<any>('https://webserv-gr4.sio-carriat.com/gsbapi/?noms='+nom).subscribe(
             (reponse) => {
-                this.medecins.push(reponse);
-                this.emitmedecinSubject();
+                this.medecins = reponse;
+                this.emitSubject();
             }
         )
     }
@@ -32,5 +36,13 @@ export class MainService{
             this.httpClient.get<any>('https://webserv-gr4.sio-carriat.com/gsbapi/?ajoutRapport='+date+"&"+motif+"&"+bilan+"&"+idVisiteur+"&"+idMedecin).subscribe();
             resolve("L'ajout dans la base de donnée à bien fonctionné");
         });
+    }
+
+    getRapportFromServer(idMedecin){
+        this.httpClient.get<any>('https://webserv-gr4.sio-carriat.com/gsbapi/?idRapport='+idMedecin).subscribe(
+            (reponse) => {
+                this.rapports = reponse;
+                this.emitSubject();
+            });
     }
 }
